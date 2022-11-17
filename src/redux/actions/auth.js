ec4060524dfe96c617e16b3eed257ac54a8b1b40
rizkirnasution <34388387+rizkirnasution@users.noreply.github.com>
2022-11-17 12:03:39 +0700

    Update auth.js

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -10,7 +10,8 @@ export const login = async (data, setErrors) => {
           "Access-Control-Allow-Origin": "*",
           'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
           'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
-        }
+        },
+        'Content-Type' : 'application/json'
       }
     );
 
